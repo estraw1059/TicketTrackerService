176ad8a3ccdf4389b8ff458299eba6c63c0afbfe
Eric <10673424+estraw1059@users.noreply.github.com>
2022-06-12 16:49:02 -0500

    Inital Commit"

---
+++ src/test/java/com/ticketTracker/ticketTracker/TicketTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketTracker.ticketTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
